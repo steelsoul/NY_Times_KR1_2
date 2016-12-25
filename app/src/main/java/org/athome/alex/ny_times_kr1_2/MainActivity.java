306b316a9622eb6164aec4ab48abb882c2c107be
Alex Pushkar <alexanderpushkar86@gmail.com>
2016-12-25 12:57:13 +0200

    Add google support

--- app/src/main/java/org/athome/alex/ny_times_kr1_2/MainActivity.java
+++ app/src/main/java/org/athome/alex/ny_times_kr1_2/MainActivity.java
@@ -1,22 +1,42 @@
 package org.athome.alex.ny_times_kr1_2;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-    Uri geoLocation;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    android.location.Location mLastLocation;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     @Override
@@ -38,22 +58,43 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(i);
                 }
             }
-                break;
+            break;
             case R.id.menu_photo: {
                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (i.resolveActivity(getPackageManager()) != null) {
                     startActivity(i);
                 }
             }
-                break;
-            case R.id.menu_map:
+            break;
+            case R.id.menu_map: {
                 Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(geoLocation);
+                Uri intentUri = Uri.parse(String.format(Locale.ENGLISH, "geo:%f,%f", mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+                i.setData(intentUri);
                 if (i.resolveActivity(getPackageManager()) != null) {
                     startActivity(i);
                 }
+            }
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
